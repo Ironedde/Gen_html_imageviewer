3d66d5a922d8ac2f0f5fbfe1a5eb9be443db70da
Edvin Åkerfeldt <edvin.akerfeldt@hotmail.com>
2016-01-04 21:57:17 +0100

    Add template

--- gen_image_viewer.py
+++ gen_image_viewer.py
@@ -22,7 +22,8 @@ def main(argv,app_name):
         elif opt in ("-o","--ofile"):
             #ADD FILENAME CHECK HERE!!
             print "ofile="+arg
-            outputfile = os.path.dirname(os.path.realpath("./"))+"/"+arg;
+            outputfile = os.path.dirname(os.path.abspath(__file__))+"/"+arg;
+            #outputfile = os.path.dirname(os.path.realpath("./"))+"/"+arg;
         else:
             assert False, "unhandled option"
     if outputfile == "":
@@ -34,13 +35,16 @@ def main(argv,app_name):
     #Begin creation
 
 
-    make_list();
+    files, dirs = make_list();
 
     #Download files if missing
     if not os.path.isfile("file_viewer.js"):
         print "Missing file: file_viewer.js"
     if not os.path.isfile("image_viewer.js"):
         print "Missing file: image_viewer.js"
+
+    make_index(outputfile,files,dirs);
+
 
 def disp_usage(app_name):
     print 'usage: '+app_name+' [options] [long options]\n type "'+app_name+' -h" for help'
@@ -57,7 +61,7 @@ def make_list():
     print files
     print dirs
     print dir_path
-
+    return (files,dirs)
 
     #for root, _, files in os.walk("./"):
     #    for f in files:
@@ -65,6 +69,22 @@ def make_list():
     #        #if os.path.getsize(fullpath) < 200 * 1024:
     #        #    os.remove(fullpath)
     #        print fullpath;
+
+def make_index(ofile,files,dirs):
+    print ofile
+    print files
+    f = open(ofile,'w');
+    with open("base_start.html") as base_start:
+        for line in base_start:
+            f.write(line)
+
+    for obj in files:
+        if ".jpg" in obj or ".gif" in obj:
+            f.write('<tr><td><a class="to_img_viewer" href="'+obj+'">'+obj+'</a></td></tr>')
+
+    with open("base_end.html") as base_end:
+        for line in base_end:
+            f.write(line)
 
 
 if __name__ == "__main__":
